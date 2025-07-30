acacc1e4282eecdc5499cbef1cc0f03e1f13aadb
gokberkgok <gokberkgok2004@gmail.com>
2025-07-30 17:32:53 +0300

    added sequelize; connection if success or error, logged message

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const dotenv = require('dotenv').config();
 const app = express();
 const customerRoutes = require('./routers/customerRoutes');
 const errorHandler = require('./middlewares/errorHandler');
+app.use(express.json());
+const sequelize = require('./db');
+
+try {
+    sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
 
 app.get('/', function (req,res) {
     res.send("hey");
@@ -10,6 +19,8 @@ app.get('/', function (req,res) {
 
 app.use('/api/customers', customerRoutes);
 app.use(errorHandler);
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
