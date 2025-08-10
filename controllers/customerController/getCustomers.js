8550b00ef4fc5a005348a616e74ceb94fbc902da
gokberkgok <gokberkgok2004@gmail.com>
2025-08-10 15:36:10 +0300

    added include section and added name and email from users table

--- controllers/customerController/getCustomers.js
+++ controllers/customerController/getCustomers.js
@@ -1,8 +1,16 @@
 const asyncHandler = require('express-async-handler');
 const Customer = require('../../models/Customer');
+const User = require('../../models/User');
 const getCustomers = asyncHandler(async (req,res) => {
+    // const userId = req.user.userId;
      const rows = await Customer.findAll({
+         // where : {user_id : userId},
           attributes: ['id', 'name', 'phone', 'email'],
+          include : [{
+               model : User,
+               as : 'user',
+               attributes : ['name', 'email']
+          }]
      });
      if (rows.length === 0){
           res.status(404).json({message : "Henüz veri yok"});
