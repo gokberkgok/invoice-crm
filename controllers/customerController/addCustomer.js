e09eac53077e2eb9fd4d4d6d42b48b6e0c30206f
gokberkgok <gokberkgok2004@gmail.com>
2025-08-10 15:35:02 +0300

    edited user_id to changed to req.user.userId

--- controllers/customerController/addCustomer.js
+++ controllers/customerController/addCustomer.js
@@ -3,7 +3,7 @@ const Customer = require('../../models/Customer');
 
 const addCustomer = asyncHandler(async (req,res) => {
      const {name, phone, email} = req.body;
-     const user_id = 1;
+     const userId = req.user.userId;
      if (!name || !phone || !email) {
           res.status(400);
           throw new Error("Name,phone,email fields are required");
@@ -14,7 +14,7 @@ const addCustomer = asyncHandler(async (req,res) => {
           res.status(400);
           throw new Error("Customer with this name already exists");
      }
-     await Customer.create({name : name, phone:phone, email:email, user_id : user_id});
+     await Customer.create({name : name, phone:phone, email:email, user_id : userId});
      res.status(201).json({ message: `Customer added : ${name}` });
 });
 
