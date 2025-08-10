3c11a4aebee47f23c21a38b949caeafe70749468
gokberkgok <gokberkgok2004@gmail.com>
2025-08-10 15:30:56 +0300

    added auth middleware

--- routers/customerRoutes.js
+++ routers/customerRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const CustomerController = require('../controllers/customerController');
 const router = express.Router();
 const customerController = new CustomerController;
+const auth = require('../middlewares/validateTokenHandler');
 
+router.use(auth);
 router.get('/', customerController.getCustomers);
 router.get('/:id', customerController.getCustomer);
 router.post('/', customerController.addCustomer);
