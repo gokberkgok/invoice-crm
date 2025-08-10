41e39258843a577dc1236a1fb60df6ea2e080664
gokberkgok <gokberkgok2004@gmail.com>
2025-08-10 15:39:58 +0300

    added verify and saved req.user datas in token

--- middlewares/validateTokenHandler.js
+++ middlewares/validateTokenHandler.js
@@ -2,21 +2,18 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const validateToken = (req, res, next) => {
-  const authHeader = req.headers.authorization;
+  const authHeader = req.headers.authorization || req.headers.Authorization;
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "Token is missing or invalid" });
   }
-
   const token = authHeader.split(" ")[1];
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded.user; // { userId: ..., name: ... }
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) return res.status(403).json({ message: "Token is not valid" });
+    req.user = decoded.user;
+    console.log("req.user:", decoded.user);  
     next();
-  } catch (error) {
-    return res.status(401).json({ message: "Token is not valid" });
-  }
+  });
 };
 
 module.exports = validateToken;
